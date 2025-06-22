1af39d5987290a3278de66a15cc12c8a104ed16f
Feras134 <145900817+Feras134@users.noreply.github.com>
2025-06-22 16:23:22 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
-// This is a program for the second homework of CSC489. Written by 	Feras Alnehabi 
-// No requirements to run the program except having JDK (Java development kit)
+
 public class Main {
     public static void main(String[] args) {
         Blockchain blockchain = new Blockchain(4); // Set difficulty level
